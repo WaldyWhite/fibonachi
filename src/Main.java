7661853292cdf338a5c25b182513baddf7c5cffa
WaldyWhite <w.weis@hotmail.com>
2023-10-04 01:58:35 +0200

    commit fibonachi

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import java.util.Arrays;
+
 public class Main {
+    public static int[] numbers = new int[30];
+
     public static void main(String[] args) {
-        var x = new isItAprimeNumber();
-        System.out.println(x.isItAprimeNumber(12));
+        // code here...
+        for ( int i = 0; i < numbers.length; i++) {
+            if ( i == 0 || i == 1) {
+                numbers[i] = 1;
+            } else if (i == 2){
+                numbers[i] = 2;
+            } else {
+                numbers[i] = numbers[i-1] + numbers[i-2];
+            }
+        }
+        System.out.println(Arrays.toString(numbers));
     }
 }
